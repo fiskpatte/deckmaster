804d40ac822fafb4e91ca57c9a68715a11a5a1b7
Pontus Lindström <pontusss85@gmail.com>
2019-07-27 15:48:54 +0200

    The menu works

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
-export default class Header extends Component {
+class Header extends Component {
+
+    handleItemClick = (e,{path}) => {
+        this.props.history.push(path)
+    }
+
     render() {
+
+        const currentPath = this.props.history.location.pathname
+        
         return (
-            <Menu>
-                <Menu.Item name='Deck officer'>
-                    <Link to="/deckofficer">Deck Officer</Link>
-                </Menu.Item>
-                <Menu.Item name='Tugmaster'>
-                    <Link to="/tugmaster">Tugmaster</Link>
-                </Menu.Item>               
+            <Menu color="blue" inverted >
+                <Menu.Item name='Deck officer' path='/deckofficer' active={currentPath.includes('/deckofficer')} onClick={this.handleItemClick}/>
+                <Menu.Item name='Tugmaster' path='/tugmaster' active={currentPath.includes('/tugmaster')} onClick={this.handleItemClick}/>
             </Menu>
         )
     }
 }
+
+export default withRouter(Header)
