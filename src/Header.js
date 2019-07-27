806b2d39321280e8be67f8866d3aff3257fc67d0
Pontus Lindström <pontusss85@gmail.com>
2019-07-27 15:11:18 +0200

    added semantic css

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <h2>Header</h2>
-            </div>
+            <Menu>
+                <Menu.Item name='Deck officer'>
+                    <Link to="/deckofficer">Deck Officer</Link>
+                </Menu.Item>
+                <Menu.Item name='Tugmaster'>
+                    <Link to="/tugmaster">Tugmaster</Link>
+                </Menu.Item>               
+            </Menu>
         )
     }
 }
