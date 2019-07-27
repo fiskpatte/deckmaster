1338934598a22b8e64d05d01064bff8f0e38c3f3
Pontus Lindström <pontusss85@gmail.com>
2019-07-27 14:53:57 +0200

    added file skeletons

---
+++ src/DeckOfficer/indec.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DeckOfficer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
