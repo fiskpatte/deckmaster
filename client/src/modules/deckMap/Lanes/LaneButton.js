a0e29b451cc1db11cdfc70cbc7a2f33d9a02cd86
Derthek <shape07_mrm@hotmail.com>
2020-03-10 15:47:36 +0100

    LaneButton drop shadow

--- client/src/modules/deckMap/Lanes/LaneButton.js
+++ client/src/modules/deckMap/Lanes/LaneButton.js
@@ -7,28 +7,16 @@ const LaneButton = ({ lane, onClick }) => {
     const originX = lane.LCG - lane.length / 2 - DECK_MAP.LANE_BUTTON_WIDTH / 2;
     const originY = lane.TCG - buttonHeight / 2;
     return (
-        <>
-            {/* <g filter="url(#filter0_d)"> */}
-            <g onClick={onClick}>
-                <rect x={originX}
-                    y={originY}
-                    width={DECK_MAP.LANE_BUTTON_WIDTH}
-                    height={buttonHeight}
-                    rx={DECK_MAP.LANE_BORDER_RADIUS}
-                    ry={DECK_MAP.LANE_BORDER_RADIUS}
-                    className="LaneButton" />
-                <ArrowIcon x={originX} y={originY} width={DECK_MAP.LANE_BUTTON_WIDTH} height={buttonHeight} />
-            </g>
-            {/* <filter id="filter0_d" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
-                <feOffset dy="4" />
-                <feGaussianBlur stdDeviation="2" />
-                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
-            </filter> */}
-        </>
+        <g onClick={onClick}>
+            <rect x={originX}
+                y={originY}
+                width={DECK_MAP.LANE_BUTTON_WIDTH}
+                height={buttonHeight}
+                rx={DECK_MAP.LANE_BORDER_RADIUS}
+                ry={DECK_MAP.LANE_BORDER_RADIUS}
+                className="LaneButton" />
+            <ArrowIcon x={originX} y={originY} width={DECK_MAP.LANE_BUTTON_WIDTH} height={buttonHeight} />
+        </g>
     );
 };
 
