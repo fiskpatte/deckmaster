4dd8ec038d92a00c0dd28ad54d561cc671278b8d
Derthek <shape07_mrm@hotmail.com>
2020-03-15 14:25:47 +0100

    main.scss for imports and start moving logic to ts

---
+++ client/src/PrivateRoute.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+import { isLoggedIn } from "./shared/functions/auth";
+
+interface PrivateRouteProps {
+  children: React.ReactNode,
+  path: string,
+  routeProps?: RouteProps
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = (props: PrivateRouteProps) => {
+  const { children, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isLoggedIn() ? (
+          children
+        ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
