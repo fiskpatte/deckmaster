2bf752830b757b62268e474703073e9634535b52
Derthek <shape07_mrm@hotmail.com>
2020-04-18 18:51:49 +0200

    add color (white, standard) prop to text

--- client/src/components/Text/Text.tsx
+++ client/src/components/Text/Text.tsx
@@ -5,6 +5,7 @@ interface Props {
   value: string;
   weight?: "light" | "standard" | "medium" | "bold";
   size?: "small" | "standard" | "medium" | "big";
+  color?: "white" | "standard";
   className?: string;
 }
 
@@ -12,11 +13,12 @@ export const Text: React.FC<Props> = ({
   value,
   weight = "standard",
   size = "standard",
+  color = "standard",
   className = "",
 }) => (
-  <div className={`Text weight-${weight} size-${size} ${className}`}>
-    {value}
-  </div>
-);
+    <div className={`Text weight-${weight} size-${size} color-${color} ${className}`}>
+      {value}
+    </div>
+  );
 
 export default Text;
