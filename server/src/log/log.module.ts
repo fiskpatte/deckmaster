21c4e2ff133b949c5ec78371f513374fa7f72277
Pontus Lindström <pontusss85@gmail.com>
2020-04-29 22:22:11 +0200

    voyage log endpoint

--- server/src/log/log.module.ts
+++ server/src/log/log.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LogService } from './log.service.';
 import { LogSchema } from './log.model';
+import { LogController } from './log.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Log', schema: LogSchema }])],
-  controllers: [],
+  controllers: [LogController],
   providers: [LogService],
   exports: [LogService],
 })
--- server/src/log/log.service..ts
+++ server/src/log/log.service..ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Log } from './log.model';
+import { transformDbModel } from 'src/utils/mongo';
 
 @Injectable()
 export class LogService {
@@ -16,5 +17,14 @@ export class LogService {
     });
 
     newLog.save();
+  }
+
+  async voyageLog(voyageId: string) {
+    try {
+      const logs = await this.logModel.find({ voyageId }).exec();
+      return logs.map(transformDbModel) as Log[];
+    } catch (error) {
+      throw error;
+    }
   }
 }
