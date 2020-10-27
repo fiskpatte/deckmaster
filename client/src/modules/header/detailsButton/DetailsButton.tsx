10c398b78053b20b0ef3dc43e2357ca18c2560df
Derthek <shape07_mrm@hotmail.com>
2020-10-27 19:12:42 +0100

    Mockup details view. Redirect correctly from cargoqueue

--- client/src/modules/header/detailsButton/DetailsButton.tsx
+++ client/src/modules/header/detailsButton/DetailsButton.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as DetailsIcon } from '../../../assets/icons/detailsIcon.svg';
-import './DetailsButtons.scss';
+import { Popup } from '../../../components/popup';
+import './DetailsButton.scss';
+import Text from "../../../components/text";
+import { Overlay } from '../../../components/overlay';
+import variables from "./DetailsButton.scss";
 
 export const DetailsButton: React.FC = () => {
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const togglePopupVisible = () => setIsPopupVisible(!isPopupVisible);
     return (
-        <div className="DetailsButton">
-            <DetailsIcon />
-        </div>
+        <>
+            <div className="DetailsButton" onClick={togglePopupVisible}>
+                <DetailsIcon />
+                <Popup visible={isPopupVisible}>
+                    <Text value={"Data not available"} />
+                </Popup>
+            </div>
+            <Overlay
+                visible={isPopupVisible}
+                transparent={true}
+                onClick={togglePopupVisible}
+                animate={false}
+                zIndex={variables.popupOverlayZIndex}
+            />
+        </>
     )
 }
 
